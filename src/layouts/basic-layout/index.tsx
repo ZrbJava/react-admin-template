1495a636adca5b559e248b0c272a117bacec0dbe
赵汝波 <rubo_zhao@asdc.com.cn>
2026-04-30 16:26:00 +0800

    ✨ feat: 重构基本布局，添加用户信息显示和登出功能，优化样式和结构

--- src/layouts/basic-layout/index.tsx
+++ src/layouts/basic-layout/index.tsx
@@ -1,8 +1,14 @@
-import type { CSSProperties } from 'react'
-import { Outlet, useLocation, useNavigate } from 'react-router-dom'
-import { Layout, Menu } from 'antd'
+import { Layout, Menu, Space, Typography, Avatar, Dropdown } from 'antd'
+import { LogoutOutlined } from '@ant-design/icons'
 import type { MenuProps } from 'antd'
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { logout } from '../../store/modules/user'
+import type { RootState, AppDispatch } from '../../store'
+
 const { Header, Sider, Content } = Layout
+const { Text } = Typography
+
 const menuItems: MenuProps['items'] = [
 	{
 		key: '/',
@@ -13,59 +19,102 @@ const menuItems: MenuProps['items'] = [
 		label: 'User List',
 	},
 ]
-const SiderStyle: CSSProperties = {
-	color: '#fff',
-	padding: 16,
-	width: 220,
-}
-const LayoutStyle: CSSProperties = {
-	height: '100vh',
-}
-const HeaderStyle: CSSProperties = {
-	height: '60px',
-	backgroundColor: '#fff',
-	borderBottom: '1px solid #f0f0f0',
-}
-const ContentStyle: CSSProperties = {
-	backgroundColor: '#f5f5f5',
-	padding: 16,
-}
-export default function BasicLayout() {
-	const navigate = useNavigate()
+
+function BasicLayout() {
 	const location = useLocation()
+	const navigate = useNavigate()
+	const dispatch = useDispatch<AppDispatch>()
+	const userInfo = useSelector((state: RootState) => state.user.userInfo)
+
+	const handleLogout = () => {
+		dispatch(logout())
+		navigate('/login')
+	}
 
 	return (
-		<>
-			<Layout style={LayoutStyle}>
-				<Sider style={SiderStyle}>
-					<div>
-						<h1>React Admin</h1>
-					</div>
-					<Menu
-						theme='dark'
-						mode='inline'
-						items={menuItems}
-						selectedKeys={[location.pathname]}
-						onClick={({ key }) => navigate(key)}
-					/>
-				</Sider>
-				<Layout style={LayoutStyle}>
-					<Header style={HeaderStyle}>Header</Header>
-					<Content style={ContentStyle}>
-						<div
-							style={{
-								height: 'calc(100vh - 100px)',
-								padding: 16,
-								background: '#fff',
-								borderRadius: 8,
-								overflow: 'auto',
+		<Layout style={{ minHeight: '100vh' }}>
+			<Sider width={220}>
+				<div
+					style={{
+						height: 56,
+						color: '#fff',
+						display: 'flex',
+						alignItems: 'center',
+						padding: '0 16px',
+						fontSize: 18,
+						fontWeight: 600,
+					}}
+				>
+					React Admin
+				</div>
+
+				<Menu
+					theme='dark'
+					mode='inline'
+					selectedKeys={[location.pathname]}
+					items={menuItems}
+					onClick={({ key }) => navigate(key)}
+				/>
+			</Sider>
+
+			<Layout>
+				<Header
+					style={{
+						background: '#fff',
+						padding: '0 16px',
+						borderBottom: '1px solid #f0f0f0',
+						display: 'flex',
+						alignItems: 'center',
+						justifyContent: 'space-between',
+					}}
+				>
+					<Text strong>Admin Console</Text>
+
+					<Space size={12}>
+						<Dropdown
+							menu={{
+								items: [
+									{
+										key: 'logout',
+										label: 'Logout',
+										onClick: handleLogout,
+										icon: <LogoutOutlined />,
+									},
+									{
+										key: 'logout2',
+										label: 'Logout2',
+										onClick: handleLogout,
+										icon: <LogoutOutlined />,
+									},
+									{
+										key: 'logout3',
+										label: 'Logout3',
+										onClick: handleLogout,
+										icon: <LogoutOutlined />,
+									},
+								],
 							}}
 						>
-							<Outlet />
-						</div>
-					</Content>
-				</Layout>
+							<Avatar size={40}>{userInfo?.username?.charAt(0)}</Avatar>
+						</Dropdown>
+					</Space>
+				</Header>
+
+				<Content style={{ padding: 16, background: '#f5f5f5' }}>
+					<div
+						style={{
+							minHeight: 'calc(100vh - 88px)',
+							padding: 16,
+							background: '#fff',
+							borderRadius: 8,
+						}}
+					>
+						<Outlet />
+					</div>
+				</Content>
 			</Layout>
-		</>
+		</Layout>
 	)
 }
+
+export default BasicLayout
